e0dd2b7cc1c4a4a12c71329ffd58b8fd5fc386e9
gustavotudeia <gustavodacostapereira@gmail.com>
2021-01-10 13:56:27 -0300

    Assinatura de metodos na intefade PessoaRepository para buscar Pessoa por email e cpf

--- src/main/java/br/com/gustavo/contaBancaria/repository/PessoaRepository.java
+++ src/main/java/br/com/gustavo/contaBancaria/repository/PessoaRepository.java
@@ -1,9 +1,17 @@
 package br.com.gustavo.contaBancaria.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.gustavo.contaBancaria.entity.Pessoa;
 
+@Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
-
+	
+	@Transactional(readOnly = true)
+	Pessoa findByEmail(String email);
+	
+	@Transactional(readOnly = true)
+	Pessoa findByCpf(String cpf);
 }
--- src/main/java/br/com/gustavo/contaBancaria/repository/TransacaoRepository.java
+++ src/main/java/br/com/gustavo/contaBancaria/repository/TransacaoRepository.java
@@ -1,9 +1,11 @@
 package br.com.gustavo.contaBancaria.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.gustavo.contaBancaria.entity.Transacao;
 
+@Repository
 public interface TransacaoRepository extends JpaRepository<Transacao, Long>{
 
 }
